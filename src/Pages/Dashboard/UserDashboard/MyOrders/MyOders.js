3a3b822b2469bfefd80e1ab72f4fd9d8a9ad6383
Sanjoy1210 <sanjoy.paul@hotmail.com>
2021-11-13 11:53:31 +0600

    user dashboade my order ready

--- src/Pages/Dashboard/UserDashboard/MyOrders/MyOders.js
+++ src/Pages/Dashboard/UserDashboard/MyOrders/MyOders.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Container, Row } from 'react-bootstrap';
+import axios from 'axios';
+import useAuth from '../../../../hooks/useAuth';
+import Order from '../Order/Order';
 
 const MyOders = () => {
+  const [myOrders, setMyOrders] = useState([]);
+  const { user } = useAuth();
+
+  useEffect(() => {
+    const loadOrders = async () => {
+      const result = await axios(`http://localhost:5000/orders?email=${user.email}`);
+      setMyOrders(result.data);
+    }
+
+    loadOrders().catch(console.dir);
+  }, [user.email]);
+
+  const handleRemoveOrder = async (id) => {
+    console.log(id);
+    const processed = window.confirm('are u sure u want to delete?');
+    if (processed) {
+      const result = await axios.delete(`http://localhost:5000/orders/${id}`);
+      if (result.data.deletedCount) {
+        alert('deleted successfully')
+      }
+      const newOrders = myOrders.filter(order => order._id !== id);
+      setMyOrders(newOrders);
+    }
+  }
+
   return (
-    <div>
-      <h2>This is my Orders</h2>
+    <div className="my-orders-page">
+      <div className="my-orders-page-wrapper">
+        <Container>
+          <Row>
+            {
+              myOrders.map(order => <Order
+                key={order._id}
+                order={order}
+                handleRemoveOrder={handleRemoveOrder}
+              />)
+            }
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 };
---
+++ src/Pages/Dashboard/UserDashboard/Order/Order.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Button, Col } from 'react-bootstrap';
+import './Order.css';
+
+const Order = ({ order, handleRemoveOrder }) => {
+  const { title, name, price, img, status, _id } = order;
+  return (
+    <Col sm={12} md={12} lg={6}>
+      <div className="single-order">
+        <img src={img} alt="" />
+        <h3>{title}</h3>
+        <span className="user-name">{name}</span>
+        <span className={status === "pending" ? "pending" : "shipped"}><p>{status}</p></span>
+        <span className="order-price">${price}</span>
+        <Button onClick={() => handleRemoveOrder(_id)} variant="warning">Cancel</Button>{' '}
+      </div>
+    </Col>
+  );
+};
+
+export default Order;
